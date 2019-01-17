96227e6cb35bf8c507b8a2131dddd2350f6eb099
hcp <suna.hing@gmail.com>
2019-01-17 12:01:35 +0900

    no message

---
+++ hcp/0hcp/02.Scripts/Heroes/HHHook.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace hcp {
+    public class HHHook : Projectile  {
+
+        enum HookState
+        {
+            Activate,
+            HookFail,
+            HookSuccess,
+            DeActivate,
+            MAX
+        }
+        [SerializeField]
+        HookState state = HookState.DeActivate;
+        [SerializeField]
+        float maxLength;
+        [SerializeField]
+        float hookVelocity;
+
+        [Tooltip("same with HeroHook's hookOriginPos property")]
+        [SerializeField]
+        Transform originPosFromheroHook;
+        [SerializeField]
+        float withDrawTime;
+        [SerializeField]
+        float withDrawVelocity;
+      
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+        }
+
+
+        public void Activate()
+        {
+            velocity = hookVelocity;
+            state = HookState.Activate;
+        }
+        public void DeActivate()
+        {
+            state = HookState.DeActivate;
+            transform.SetPositionAndRotation(originPosFromheroHook.position, originPosFromheroHook.rotation);
+        }
+        public void HookFail()
+        {
+            state = HookState.HookFail;
+        }
+
+        private void Update()
+        {
+            switch (state)
+            {
+                case HookState.Activate:
+                    transform.Translate(Vector3.back * withDrawVelocity * Time.deltaTime * withDrawTime, Space.Self);
+
+
+                    break;
+                case HookState.HookFail:
+                    transform.Translate(Vector3.back * withDrawVelocity * Time.deltaTime * withDrawTime, Space.Self);
+                    if (transform.localPosition.z + Mathf.Epsilon < originPosFromheroHook.localPosition.z)
+                    {
+                        state = HookState.DeActivate;
+                        if(attachingHero.photonView.IsMine)
+                        attachingHero.photonView.RPC("HookIsDone", Photon.Pun.RpcTarget.All);
+                    }
+                    break;
+            }
+        }
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!attachingHero.photonView.IsMine) return;
+            if (state != HookState.Activate) return;
+
+            int layer = other.gameObject.layer;
+            if (layer == Constants.mapLayerMask)
+            {
+                attachingHero.photonView.RPC("HookFailed", Photon.Pun.RpcTarget.All);
+            }
+            if (TeamInfo.GetInstance().IsThisLayerEnemy(layer))
+            {
+
+            }
+        }
+        
+    }
+}
---
+++ hcp/0hcp/02.Scripts/Heroes/HeroHook.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using Photon.Pun;
+
+namespace hcp
+{
+    public class HeroHook : Hero
+    {
+        [Space(20)]
+        [Header("Hero - Hook's Property")]
+        [Space(10)]
+        [SerializeField]
+        float normalAttackLength;
+        [SerializeField]
+        float normalAttackLengthDiv;
+        [SerializeField]
+        float correctionRange;
+        [SerializeField]
+        float correctionRangeSqr;
+        [SerializeField]
+        float normalAttackDamage;
+        [SerializeField]
+        float normalAttackFireRate;
+
+        [Space(10)]
+        [Header("   Hero - Hook - First Skill Hook")]
+        [SerializeField]
+        HHHook hookProjectile;
+        [SerializeField]
+        Transform hookOriginPos;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            normalAttackLengthDiv = 1 / normalAttackLength;
+            correctionRangeSqr = correctionRange * correctionRange;
+        }
+        protected override void SetActiveCtrls()
+        {
+            base.SetActiveCtrls();
+
+        }
+
+        #region Basic Control
+
+        #endregion
+        
+        #region NormalAttack
+        void NormalAttack()
+        {
+            List<Hero> enemyHeroes = TeamInfo.GetInstance().EnemyHeroes;
+            Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+            Vector3 normalAttackVector = ray.direction * normalAttackLength;
+
+            for (int i = 0; i < enemyHeroes.Count; i++)
+            {
+                Hero enemy = enemyHeroes[i];
+                Vector3 enemyPosition = enemy.CenterPos - ray.origin;
+                float dot = Vector3.Dot(enemyPosition, normalAttackVector);
+                if (dot < Mathf.Epsilon)
+                {
+                    Debug.Log(enemy.photonView.ViewID+ "HH NA enemy Behind, no attack");
+                    continue;
+                }
+                float projectedDis = dot * normalAttackLengthDiv;
+                if (projectedDis > normalAttackLength)
+                {
+                    Debug.Log(enemy.photonView.ViewID + "HH NA enemy too far, no attack");
+                    continue;
+                }
+                float projectedDisSqr = projectedDis * projectedDis;
+                float orthogonalDisSqr = enemyPosition.sqrMagnitude - projectedDisSqr;
+                if (orthogonalDisSqr > correctionRangeSqr)
+                {
+                    Debug.Log(enemy.photonView.ViewID + "HH NA enemy orthogonalDis too far, no attack");
+                    continue;
+                }
+                enemy.photonView.RPC("GetDamaged", Photon.Pun.RpcTarget.All, normalAttackDamage);
+            }
+        }
+        #endregion
+
+        #region FirstSkill - Hook
+
+        void DoHook()
+        {
+            //자가 정지 시키기.
+            Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+            hookOriginPos.transform.LookAt(ray.direction * maxShotLength);
+            photonView.RPC("ActivateHook", RpcTarget.All);
+        }
+        [PunRPC]
+        public void ActivateHook()
+        {
+            //후킹은 자가 정지 후에 발동하는 로직이니까 훅의 위치 동기화 까지는 필요 없을듯
+            hookProjectile.Activate();
+        }
+        [PunRPC]
+        public void HookFailed()
+        {
+            //후킹 실패로 훅이 돌아오는 타이밍일뿐
+            hookProjectile.HookFail();
+        }
+        [PunRPC]
+        public void HookIsDone()
+        {
+            hookProjectile.DeActivate();
+        }
+
+        #endregion
+
+        #region Ultimate
+
+        #endregion
+
+    }
+}
