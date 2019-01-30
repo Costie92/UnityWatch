060c1e6a4c98c01a015d3f1db9927ba3cb211695
Costie <costie92@gmail.com>
2019-01-30 15:56:33 +0900

    quit

---
+++ Assets/Costie/02. Script/QuitButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour {
+
+    [SerializeField] private Button button;
+    // Use this for initialization
+    void Start()
+    {
+        button = this.GetComponent<Button>();
+        button.onClick.AddListener(onClickQuit);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+    }
+    public void onClickQuit()
+    {
+        if (Application.platform == RuntimePlatform.WindowsEditor) {
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+}
